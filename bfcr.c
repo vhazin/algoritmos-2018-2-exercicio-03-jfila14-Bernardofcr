e639e1a5d2cb8f618addafe397a9596a9e425901
bernardo russo <bfcr@cesar.school>
2018-09-02 11:48:58 -0300

    comentado

--- bfcr.c
+++ bfcr.c
@@ -15,38 +15,38 @@ typedef struct pessoa{
 }pessoaIn;
 
 typedef struct Fila{
-    int total;
-    struct pessoa *primeiro;
-    struct pessoa *ultimo;
+    int total;//total de elementos na lista
+    struct pessoa *primeiro;//ponteiro para primeiro elemento
+    struct pessoa *ultimo;//ponteiro para ultimo elemento
 };
 
 void addlista(struct Fila *lista, int add){
     struct pessoa *novo=(pessoaIn*)malloc(sizeof(struct pessoa)); //alocar espaço subsequente na memória
-    novo->id=add;
-    novo->proximo=NULL;
-    if(lista->primeiro==NULL){
+    novo->id=add;//adiciona valor de add para id de pessoa
+    novo->proximo=NULL;//zera proximo ponteiro
+    if(lista->primeiro==NULL){//ajusta valores para lista que antes não tinha itens
         lista->primeiro=novo;
         lista->ultimo=novo;
         return;
     }
-    lista->ultimo->proximo=novo;
+    lista->ultimo->proximo=novo;//mudança de posições na lista
     lista->ultimo=novo;
     return;
 }
 
 void dellista(struct Fila *lista, int del){
     struct pessoa *ant = NULL, *aux;
-    aux=lista->primeiro;
-    while (aux!=NULL){
+    aux=lista->primeiro;//define aux para inicio de teste da fila
+    while (aux!=NULL){//while roda até chegar no ponteiro que aponta para nulo
         if (aux->id==del){
-            if (ant==NULL)
-                lista->primeiro=aux->proximo;
+            if (ant==NULL)//caso valor anterior seja nulo
+                lista->primeiro=aux->proximo;//transfere valor proximo valor para começo
             else
-                ant->proximo=aux->proximo;
-            lista->total=(lista->total)-1;
+                ant->proximo=aux->proximo;//se não anterior vai receber valor de proximo
+            lista->total=(lista->total)-1;//reduz tamanho da lista
             return;
         }
-        ant=aux;
+        ant=aux;//dar seguimento ao while as duas funções fazem a lista caminhar
         aux=aux->proximo;
     }
     return;
@@ -57,23 +57,23 @@ int main(void) {
     fila=malloc(sizeof(struct Fila));
     int n, n0=0, codigo, m = 0, m0=0; //n = quantidade de pessoas
     scanf("%d",&n); //receber quantidade de pessoas
-    fila->total=n;
+    fila->total=n;//transferir total para lista
     while (n0<n){
-        scanf("%d",&codigo);
-        addlista(fila,codigo);
+        scanf("%d",&codigo);//receber codigo
+        addlista(fila,codigo);//puchar função de adicionar valor na lista
         n0++;
     }
     scanf("%d",&m);
     while (m0<m){
-        scanf ("%d",&codigo);
-        dellista(fila,codigo);
+        scanf ("%d",&codigo);//receber codigo
+        dellista(fila,codigo);//puchar função de varrer lista e deletar codigo
         m0++;
     }
-    int r=n-m;
-    struct pessoa *print=fila->primeiro;
+    int r=n-m;//definir número final de elementos na lista
+    struct pessoa *print=fila->primeiro;//definir struct para printar pessoas
     while (r--){
-        printf("%d ", print->id);
-        print=print->proximo;
+        printf("%d ", print->id);//printar item atual
+        print=print->proximo;//ir ao proximo item
     }
     printf("\n");
     
